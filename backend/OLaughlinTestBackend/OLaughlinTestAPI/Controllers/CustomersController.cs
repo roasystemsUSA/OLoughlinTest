52e079c4cba37bf9776dd03723474ae5ddf780ad
roasystemsUSA <roasystems@gmail.com>
2025-09-12 22:51:13 -0700

    Changes to the code to include the Controller for the Customers

---
+++ backend/OLaughlinTestBackend/OLaughlinTestAPI/Controllers/CustomersController.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.Base;
+using Models.Entities;
+using Services;
+namespace OLaughlinTestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // GET: api/<CustomersController>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<BaseResponseModel<IEnumerable<Customer>>> GetAllAsync()
+        {
+            try
+            {
+                var customers = await _customerService.GetAllAsync();
+                if (customers == null || !customers.Any())
+                {
+                    return new BaseResponseModel<IEnumerable<Customer>>()
+                    {
+                        ErrorDetails = new ErrorDetailsModel()
+                        {
+                            ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                            HttpStatusCode = HttpStatusCodeEnum.NotFound,
+                            Message = "No customers found"
+                        },
+                        HasError = false,
+                        Result = null
+                    };
+                }
+                return new BaseResponseModel<IEnumerable<Customer>>()
+                {
+                    Result = customers,
+                    ErrorDetails = new ErrorDetailsModel() { HttpStatusCode = HttpStatusCodeEnum.OK, Message = "Success" },
+                    HasError = false
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponseModel<IEnumerable<Customer>>()
+                {
+                    ErrorDetails = new ErrorDetailsModel()
+                    {
+                        ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                        HttpStatusCode = HttpStatusCodeEnum.Internal,
+                        Message = ex.Message
+                    },
+                    HasError = true,
+                    Result = null
+                };
+            }
+        }
+
+        // GET: api/Customer/id
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<BaseResponseModel<Customer>> GetCustomer(string id)
+        {
+            try
+            {
+                var result = await _customerService.GetByIdAsync(id);
+                if (result == null)
+                {
+                    return new BaseResponseModel<Customer>()
+                    {
+                        ErrorDetails = new ErrorDetailsModel()
+                        {
+                            ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                            HttpStatusCode = HttpStatusCodeEnum.NotFound,
+                            Message = "Customer not found"
+                        },
+                        HasError = true,
+                        Result = null
+                    };
+                }
+                // Create a response model
+                BaseResponseModel<Customer> response = new BaseResponseModel<Customer>()
+                {
+                    Result = result,
+                    ErrorDetails = new ErrorDetailsModel() { HttpStatusCode = HttpStatusCodeEnum.OK, Message = "Success" },
+                    HasError = false
+                };
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponseModel<Customer>()
+                {
+                    ErrorDetails = new ErrorDetailsModel()
+                    {
+                        ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                        HttpStatusCode = HttpStatusCodeEnum.Internal,
+                        Message = ex.Message
+                    },
+                    HasError = true,
+                    Result = null
+                };
+            }
+        }
+
+
+        // POST: api/Customer
+        [HttpPost]
+        public async Task<BaseResponseModel<Customer>> PostCustomer(Customer customer)
+        {
+            try
+            {
+
+                // Add the customer to the database
+                var insertedCustomer = await _customerService.AddAsync(customer);
+
+
+                BaseResponseModel<Customer> result = new BaseResponseModel<Customer>()
+                {
+                    Result = insertedCustomer,
+                    ErrorDetails = new ErrorDetailsModel() { HttpStatusCode = HttpStatusCodeEnum.OK, Message = "Success" },
+                    HasError = false
+                };
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponseModel<Customer>()
+                {
+                    ErrorDetails = new ErrorDetailsModel()
+                    {
+                        ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                        HttpStatusCode = HttpStatusCodeEnum.Internal,
+                        Message = $"An error occurred while creating the customer. Details: {ex.Message}"
+                    },
+                    HasError = true,
+                    Result = null
+                };
+            }
+        }
+
+
+        // PUT: api/Customer/id
+        [HttpPut("{id}")]
+        public async Task<BaseResponseModel<bool?>> PutCustomer(Customer customer)
+        {
+            try
+            {
+                // update the existing customer
+                var updateResult = await _customerService.UpdateAsync(customer);
+                BaseResponseModel<bool?> result = new BaseResponseModel<bool?>()
+                {
+                    Result = updateResult,
+                    ErrorDetails = new ErrorDetailsModel() { HttpStatusCode = HttpStatusCodeEnum.OK, Message = "Success" },
+                    HasError = !updateResult
+                };
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponseModel<bool?>()
+                {
+                    ErrorDetails = new ErrorDetailsModel()
+                    {
+                        ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                        HttpStatusCode = HttpStatusCodeEnum.Internal,
+                        Message = $"An error occurred while updating the customer. Details: {ex.Message}"
+                    },
+                    HasError = true,
+                    Result = null
+                };
+            }
+
+
+        }
+
+        /// <summary>
+        /// Deletes an Customer
+        /// </summary>
+        /// <param name="id">Customer ID</param>
+        /// <returns>Boolean indicating success or failure of the delete operation</returns>
+        [HttpDelete("{id}")]
+        public async Task<BaseResponseModel<bool?>> DeleteCustomer(string id)
+        {
+            try
+            {
+                bool deleteResult;
+
+                // Perform physical delete logic
+                deleteResult = await _customerService.DeleteAsync(id);
+                if (!deleteResult)
+                {
+                    return new BaseResponseModel<bool?>()
+                    {
+                        ErrorDetails = new ErrorDetailsModel()
+                        {
+                            ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                            HttpStatusCode = HttpStatusCodeEnum.NotFound,
+                            Message = $"Customer with ID {id} was not found for physical deletion."
+                        },
+                        HasError = true,
+                        Result = false
+                    };
+                }
+                return new BaseResponseModel<bool?>()
+                {
+                    Result = deleteResult,
+                    ErrorDetails = new ErrorDetailsModel()
+                    {
+                        HttpStatusCode = HttpStatusCodeEnum.OK,
+                        Message = deleteResult
+                            ? $"Customer successfully deleted."
+                            : $"Failed to  delete the customer."
+                    },
+                    HasError = !deleteResult
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponseModel<bool?>()
+                {
+                    ErrorDetails = new ErrorDetailsModel()
+                    {
+                        ErrorType = ErrorDetailsModel.ErrorTypeEnum.Internal,
+                        HttpStatusCode = HttpStatusCodeEnum.Internal,
+                        Message = $"An error occurred while deleting the customer. Details: {ex.Message}"
+                    },
+                    HasError = true,
+                    Result = null
+                };
+            }
+        }
+    }
+}
--- backend/OLaughlinTestBackend/OLaughlinTestAPI/Program.cs
+++ backend/OLaughlinTestBackend/OLaughlinTestAPI/Program.cs
@@ -1,30 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Repositories;
+using Services;
 
 namespace OLaughlinTestAPI
 {
     public class Program
     {
+        public class Environment
+        {
+            public string Name { get; set; }
+            public string ConnectionString { get; set; }
+        }
+
+        private static string GetConnectionStringForEnvironment(IConfiguration configuration, string environmentMode)
+        {
+            var environments = configuration.GetSection("Environments").Get<List<Environment>>();
+            var environment = environments?.FirstOrDefault(x => x.Name == environmentMode);
+
+            if (environment == null || string.IsNullOrEmpty(environment.ConnectionString))
+            {
+                throw new Exception($"Connection string for environment '{environmentMode}' not found.");
+            }
+
+            return configuration.GetConnectionString(environment.ConnectionString);
+        }
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            // 🗃️ DbContexts
+            var environmentMode = builder.Configuration["Environment"] ?? "Development";
+            var connectionString = GetConnectionStringForEnvironment(builder.Configuration, environmentMode);
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            // 💼 Application Services and Repositories
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+            builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            // Services
+            builder.Services.AddScoped<ICustomerService, CustomerService>();
+            builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
-            // Add services to the container.
+            // 🌐 CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
 
+            // 📘 Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OLaughlin.API.Demo.Local", Version = "v1" });
+            });
+
+            // 📦 Controllers
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseCors("AllowAll");
 
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapControllers();
 
